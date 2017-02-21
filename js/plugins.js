cc694955d824b5404fbadbd2c063efc54ca2b4b0
Hariharan Panneerselvam <Hariharan_P03@ad.infosys.com>
2017-02-21 12:18:43 +0530

    code src block test

--- js/plugins.js
+++ js/plugins.js
@@ -258,5 +258,17 @@ $(function() {
       e.preventDefault();
   });
   $(document).find("footer").hide();
-
+  $(window).on('keydown', function (event) {
+      if (event.keyCode == 123) {
+          return false;
+      }
+      else if (event.ctrlKey && event.shiftKey && (event.keyCode == 73 || event.keyCode == 67 || event.keyCode == 85)) {
+          
+          return false;  //Prevent from ctrl+shift+i
+      }
+      else if (event.ctrlKey && (event.keyCode == 73 || event.keyCode == 67 || event.keyCode == 85)) {
+          
+          return false;  //Prevent from ctrl+shift+i
+      }
+  });
 }); // end of document ready
